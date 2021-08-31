b994918b859e507ebd1ff99f72aba61346e9136e
JustinChaokekw <89628557+JustinChaokekw@users.noreply.github.com>
2021-08-31 13:51:31 +0800

    letter uwu

---
+++ BasicsAndPrinting/LetterToYourself/LetterToYourself.java
@@ -0,0 +1,20 @@
+public class LetterToYourself
+{
+  public static void main( String[] args )
+  {
+    System.out.println( "U:\>java LetterToYourself" );
+    System.out.println( "                                                    " );
+    System.out.println( "+--------------------------------------------------+" );
+    System.out.println( "|                                              ####|" );
+    System.out.println( "|                                              ####|" );
+    System.out.println( "|                                              ####|" );
+    System.out.println( "|                                                  |" );
+    System.out.println( "|                                                  |" );
+    System.out.println( "|                Justin Chao                       |" );
+    System.out.println( "|                No. 72, Beida Rd.                 |" );
+    System.out.println( "|                Hsinchu City, 30044               |" );
+    System.out.println( "|                                                  |" );
+    System.out.println( "+--------------------------------------------------+" );
+    // I don't know whether this will actually work or not, I hope it does.
+  }
+}
