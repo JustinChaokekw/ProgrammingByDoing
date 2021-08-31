6f7a84d46c4d95d5769188b8bc83da3df2a783b8
JustinChaokekw <89628557+JustinChaokekw@users.noreply.github.com>
2021-08-31 17:12:39 +0800

    also on the car

---
+++ Variables/StillUsingVariables/StillUsingVariables.java
@@ -0,0 +1,14 @@
+// Justin Chao. August 31th, 2021.
+public class StillUsingVariables
+{
+  public static void main( String[] args )
+  {
+    String Name;
+    int GraduateYear;
+
+    Name = "Justin Chao";
+    GraduateYear = 2024;
+
+    System.out.println( "My name is " + Name + " and I'll graduate in " + GraduateYear + ".");
+  }
+}
