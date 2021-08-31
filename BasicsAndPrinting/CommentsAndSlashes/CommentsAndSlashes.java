8826c1ad19d729b2908a751cc78dc5d36dd7db65
JustinChaokekw <89628557+JustinChaokekw@users.noreply.github.com>
2021-08-31 13:39:15 +0800

    umm, the class file didn't pop out so i made it save itself in .class, i dunno is it okay or not

---
+++ BasicsAndPrinting/CommentsAndSlashes/CommentsAndSlashes.java
@@ -0,0 +1,17 @@
+// Justin Chao. August 31th, 2021.
+public class CommentsAndSlashes
+// I mean, the title is literally "CommentsAndSlashes", it's kinda easy to know that the two slashes relate to the commenting function.
+{
+  public static void main( String[] args )
+  {
+    // A comment, this is so you can read your program later.
+    // Anything after the // is ignored by Java.
+
+    System.out.println( "Ii could have code like this." ); // an the comment after is ignored.
+
+    // You can also use a comment to "disable" or comment out a piece of code:
+    // System.out.println("This won't run.");
+
+    System.out.println( "This will run." )
+  }
+}
