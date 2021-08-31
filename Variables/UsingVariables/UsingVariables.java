dfa3ac7003e7901c9930e4baef5346238c863ccd
JustinChaokekw <89628557+JustinChaokekw@users.noreply.github.com>
2021-08-31 17:05:13 +0800

    i did this on a car, feeling dizzy

---
+++ Variables/UsingVariables/UsingVariables.java
@@ -0,0 +1,18 @@
+// Justin Chao. August 31th, 2021
+public class UsingVariables
+{
+  public static void main( String[] args )
+  {
+    String Subject;
+    int RoomNumber;
+    double Value;
+
+    Subject = "Computer Science";
+    RoomNumber = 113;
+    Value = 2.71828;
+
+    System.out.println( "This is room # " + RoomNumber );
+    System.out.println( "e is close to " + Value );
+    System.out.println( "I am learning a bit about" + Subject );
+  }
+}
