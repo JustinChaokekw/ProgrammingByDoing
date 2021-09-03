a56f2a24210e18c92a703bdca9e018e07127ae87
JustinChaokekw <89628557+JustinChaokekw@users.noreply.github.com>
2021-09-03 13:54:10 +0800

    konjibhuvgycft

---
+++ IfStatements/ModulusAnimation/ModulusAnimation.java
@@ -0,0 +1,106 @@
+public class ModulusAnimation
+{
+	public static void main( String[] args ) throws Exception
+	{
+		for ( int i=0; i<150; i++ )
+		{
+			if ( i%50 == 0 )
+				System.out.println("                         ");
+      else if ( i%50 == 1 )
+  			System.out.println("                        N");
+      else if ( i%50 == 2 )
+    		System.out.println("                       NE");
+      else if ( i%50 == 3 )
+      	System.out.println("                      NEX");
+      else if ( i%50 == 4 )
+        System.out.println("                     NEXT");
+      else if ( i%50 == 5 )
+        System.out.println("                    NEXT ");
+      else if ( i%50 == 6 )
+        System.out.println("                   NEXT S");
+      else if ( i%50 == 7 )
+        System.out.println("                  NEXT ST");
+      else if ( i%50 == 8 )
+        System.out.println("                 NEXT STA");
+      else if ( i%50 == 9 )
+        System.out.println("                NEXT STAT");
+      else if ( i%50 == 10 )
+        System.out.println("               NEXT STATI");
+      else if ( i%50 == 11 )
+        System.out.println("              NEXT STATIO");
+      else if ( i%50 == 12 )
+        System.out.println("             NEXT STATION");
+      else if ( i%50 == 13 )
+        System.out.println("            NEXT STATION:");
+      else if ( i%50 == 14 )
+        System.out.println("           NEXT STATION: ");
+      else if ( i%50 == 15 )
+        System.out.println("          NEXT STATION: u");
+      else if ( i%50 == 16 )
+        System.out.println("         NEXT STATION: un");
+      else if ( i%50 == 17 )
+        System.out.println("        NEXT STATION: unk");
+      else if ( i%50 == 18 )
+        System.out.println("       NEXT STATION: unkn");
+      else if ( i%50 == 19 )
+        System.out.println("      NEXT STATION: unkno");
+      else if ( i%50 == 20 )
+        System.out.println("     NEXT STATION: unknow");
+      else if ( i%50 == 21 )
+        System.out.println("    NEXT STATION: unknown");
+      else if ( i%50 == 22 )
+        System.out.println("   NEXT STATION: unknown ");
+      else if ( i%50 == 23 )
+        System.out.println("  NEXT STATION: unknown  ");
+      else if ( i%50 == 24 )
+        System.out.println(" NEXT STATION: unknown   ");
+      else if ( i%50 == 25 )
+        System.out.println("NEXT STATION: unknown    ");
+      else if ( i%50 == 26 )
+        System.out.println("EXT STATION: unknown     ");
+      else if ( i%50 == 27 )
+        System.out.println("XT STATION: unknown      ");
+      else if ( i%50 == 28 )
+        System.out.println("T STATION: unknown       ");
+      else if ( i%50 == 29 )
+        System.out.println(" STATION: unknown        ");
+      else if ( i%50 == 30 )
+        System.out.println("STATION: unknown         ");
+      else if ( i%50 == 31 )
+        System.out.println("TATION: unknown          ");
+      else if ( i%50 == 32 )
+        System.out.println("ATION: unknown           ");
+      else if ( i%50 == 33 )
+        System.out.println("TION: unknown            ");
+      else if ( i%50 == 34 )
+        System.out.println("ION: unknown             ");
+      else if ( i%50 == 35 )
+        System.out.println("ON: unknown              ");
+      else if ( i%50 == 36 )
+        System.out.println("N: unknown               ");
+      else if ( i%50 == 37 )
+        System.out.println(": unknown                ");
+      else if ( i%50 == 38 )
+        System.out.println(" unknown                 ");
+      else if ( i%50 == 39 )
+        System.out.println("unknown                  ");
+      else if ( i%50 == 40 )
+        System.out.println("nknown                   ");
+      else if ( i%50 == 41 )
+        System.out.println("known                    ");
+      else if ( i%50 == 42 )
+        System.out.println("nown                     ");
+      else if ( i%50 == 43 )
+        System.out.println("own                      ");
+      else if ( i%50 == 44 )
+        System.out.println("wn                       ");
+      else if ( i%50 == 45 )
+        System.out.println("n                        ");
+      else
+        System.out.println("                         ");
+
+			Thread.sleep(200);
+		}
+
+	}
+}
